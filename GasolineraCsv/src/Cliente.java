75fd7e51612e422819062fabc1d454709b9de2a3
Alejandro Guillén <grucoliak@gmail.com>
2026-09-22 09:17:57 +0200

    Commit entrega parcial

--- GasolineraCsv/src/Cliente.java
+++ GasolineraCsv/src/Cliente.java
@@ -1,4 +1,7 @@
-public class Cliente {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Cliente implements Comparable<Cliente>{
     private final int ID;
     private String nombre;
     private String telefono;
@@ -9,6 +12,26 @@ public class Cliente {
         this.nombre = nombre;
         this.telefono = telefono;
         this.matricula = matricula;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Cliente cliente)) return false;
+        return ID == cliente.ID && Objects.equals(nombre, cliente.nombre) && Objects.equals(telefono, cliente.telefono) && Objects.equals(matricula, cliente.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID, nombre, telefono, matricula);
+    }
+
+    @Override
+    public int compareTo(Cliente o) {
+        int comparacion = nombre.compareToIgnoreCase(o.getNombre());
+        if (comparacion == 0) {
+            comparacion = this.ID - o.getID();
+        }
+        return comparacion;
     }
 
     public int getID() {
