47274144a0eb49f5759fe52c32a2e3340fdd95a8
Alejandro Guillén <grucoliak@gmail.com>
2026-09-25 09:57:00 +0200

    Método obtenerTodos implementado

--- GasolineraCsv/src/ClienteCsvAlmacenamiento.java
+++ GasolineraCsv/src/ClienteCsvAlmacenamiento.java
@@ -1,7 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class ClienteCsvAlmacenamiento implements Almacenamiento<Cliente> {
+    private final Path directorio;
+    private final Path fichero;
+
+    public ClienteCsvAlmacenamiento() {
+        this.directorio = Path.of("datos");
+        this.fichero = directorio.resolve("clientes.csv");
+    }
 
     @Override
     public void guardar(Cliente entidad) {
@@ -10,6 +23,29 @@ public class ClienteCsvAlmacenamiento implements Almacenamiento<Cliente> {
 
     @Override
     public Collection<Cliente> obtenerTodos() {
-        return List.of();
+        Collection<Cliente> clientes = new ArrayList<>();
+
+        try (BufferedReader reader = Files.newBufferedReader(fichero)) {
+            String linea = reader.readLine(); // lee el header del archivo csv y evita que lo consuma el bucle
+
+            while ((linea = reader.readLine()) != null) {
+                linea = linea.substring(0, linea.indexOf(";")); // Busca el último carácter de la línea (;) y lo elimina, incluyendo lo posterior
+                String[] atributosCliente = linea.split(",");
+
+                Cliente c = new Cliente(
+                        Integer.parseInt(atributosCliente[0]),
+                        atributosCliente[1],
+                        atributosCliente[2],
+                        atributosCliente[3]
+                );
+
+                clientes.add(c);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Holi esto no va :c (leer fichero clientes)");
+        }
+
+        return clientes;
     }
 }
